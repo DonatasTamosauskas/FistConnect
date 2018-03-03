edcbaabb10cd7b430018fd290672c40a08f1f08e
Dom <domantasgiedraitis@gmail.com>
2018-03-03 11:20:59 +0200

    Added matchStart onClick.

---
+++ app/src/main/java/com/example/android/fistconnect/CurrentUser.java
@@ -0,0 +1,24 @@
+package com.example.android.fistconnect;
+
+import android.widget.ImageView;
+
+/**
+ * Created by Domantas on 2018-03-03.
+ */
+//Dummy class
+public class CurrentUser {
+
+    public ImageView imageView;
+    public String username;
+    public int level;
+    public boolean isChallenged;
+
+    public CurrentUser() {
+        username = "DabartinisUser";
+        level = 20;
+        isChallenged = true;
+    }
+
+
+
+}
---
+++ app/src/main/java/com/example/android/fistconnect/ListActivity.java
@@ -0,0 +1,43 @@
+package com.example.android.fistconnect;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        //Create current user adapter
+        ArrayList<Enemy> arrayOfUsers = new ArrayList<>();
+        EnemyObjectAdapter adapter = new EnemyObjectAdapter(this, arrayOfUsers);
+        ListView listView = (ListView) findViewById(R.id.display_currentUser);
+        listView.setAdapter(adapter);
+
+        Enemy e = new Enemy();
+        adapter.add(e);
+
+        //Create adapter for enemies
+        ArrayList<Enemy> arrayOfEnemies = new ArrayList<>();
+        EnemyObjectAdapter enemyAdapter = new EnemyObjectAdapter(this, arrayOfEnemies);
+        ListView enemyListView = (ListView) findViewById(R.id.display_enemies);
+        enemyListView.setAdapter(enemyAdapter);
+
+        for (int i = 0; i < 10; i++) {
+            Enemy f = new Enemy();
+            enemyAdapter.add(f);
+        }
+    }
+
+    public void startMatch(View v) {
+        
+    }
+}
